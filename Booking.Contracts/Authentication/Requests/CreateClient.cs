9ffa6c22bec38056feb8de8ae077d50dde52fecb
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-02-07 20:17:19 +0500

    feat: Добавила контракты для создания клиента

---
+++ Booking.Contracts/Authentication/Requests/CreateClient.cs
@@ -0,0 +1,11 @@
+﻿using Otus.Booking.Common.Booking.Contracts.Authentication.Models;
+
+namespace Otus.Booking.Common.Booking.Contracts.Authentication.Requests;
+
+/// <summary>
+/// Запрос на создание клиента
+/// </summary>
+public record CreateClient : BaseClientDto
+{
+    
+}
---
+++ Booking.Contracts/Authentication/Responses/CreateClientResult.cs
@@ -0,0 +1,11 @@
+﻿using Otus.Booking.Common.Booking.Contracts.Authentication.Models;
+
+namespace Otus.Booking.Common.Booking.Contracts.Authentication.Responses;
+
+/// <summary>
+/// Ответ на успешное создание клиента
+/// </summary>
+public record CreateClientResult : BaseClientDto
+{
+    public Guid Id { get; set; }
+}
