d34f58c53e7851988018075ca93ac7b7647da243
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-02-11 18:24:13 +0500

    feat: Добавила контракты для обновления пользователя

---
+++ Booking.Contracts/Authentication/Requests/UpdateUser.cs
@@ -0,0 +1,11 @@
+﻿using Otus.Booking.Common.Booking.Contracts.Authentication.Models;
+
+namespace Otus.Booking.Common.Booking.Contracts.Authentication.Requests;
+
+/// <summary>
+/// Запрос на обновление пользователя
+/// </summary>
+public record UpdateUser : BaseUserDto
+{
+    public Guid Id { get; set; }
+}
---
+++ Booking.Contracts/Authentication/Responses/UpdateUserResult.cs
@@ -0,0 +1,11 @@
+﻿using Otus.Booking.Common.Booking.Contracts.Authentication.Models;
+
+namespace Otus.Booking.Common.Booking.Contracts.Authentication.Responses;
+
+/// <summary>
+/// Ответ на успешное обнолвление пользователя
+/// </summary>
+public record UpdateUserResult : BaseUserDto
+{
+    public Guid Id { get; set; }
+}
