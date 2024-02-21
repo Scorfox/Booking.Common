95af9e7a903985787d4a8106d338467664c4819f
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-02-21 08:07:49 +0500

    WhoConfirmedId Nullable

--- Booking.Contracts/Reservation/Models/BaseReservationDto.cs
+++ Booking.Contracts/Reservation/Models/BaseReservationDto.cs
@@ -5,7 +5,7 @@ public abstract record BaseReservationDto
     public Guid TableId { get; set; }
     
     public Guid WhoBookedId { get; set; }
-    public Guid WhoConfirmedId { get; set; }
+    public Guid? WhoConfirmedId { get; set; }
     public Guid? WhoCancelledId { get; set; }
     
     public DateTimeOffset From { get; set; }
