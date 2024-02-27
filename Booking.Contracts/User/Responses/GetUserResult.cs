8435e9f90d899dea201798186a6cbb2b12318e2b
Haxend <dunaevskyilya@gmail.com>
2024-02-27 22:11:57 +0300

    Create GetUserResult.cs

---
+++ Booking.Contracts/User/Responses/GetUserResult.cs
@@ -0,0 +1,9 @@
+﻿using Otus.Booking.Common.Booking.Contracts.User.Models;
+
+namespace Otus.Booking.Common.Booking.Contracts.User.Responses
+{
+    public record GetUserResult : BaseUserDto
+    {
+        public Guid Id { get; set; }
+    }
+}
