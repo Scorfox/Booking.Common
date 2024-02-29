9f7c8bf02f229195147f668aab402454b0943994
Haxend <dunaevskyilya@gmail.com>
2024-03-01 02:02:52 +0300

    Create GetTableResult.cs

---
+++ Booking.Contracts/Table/Responses/GetTableResult.cs
@@ -0,0 +1,9 @@
+﻿using Otus.Booking.Common.Booking.Contracts.Table.Models;
+
+namespace Otus.Booking.Common.Booking.Contracts.Table.Responses
+{
+    public record GetTableResult : FullTableDto
+    {
+
+    }
+}
