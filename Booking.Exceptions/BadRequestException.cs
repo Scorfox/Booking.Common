028532d68f97747137b1642a59d66a44f6a09c66
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-03-02 12:45:21 +0500

    feat: Добавила конструктор с массивом ошибок для BadRequestException

--- Booking.Exceptions/BadRequestException.cs
+++ Booking.Exceptions/BadRequestException.cs
@@ -5,4 +5,11 @@ public class BadRequestException : Exception
     public BadRequestException(string message) : base(message)
     {
     }
-}
+
+    public BadRequestException(string[] errors) : base("Multiple errors occurred. See error details.")
+    {
+        Errors = errors;
+    }
+
+    public string[] Errors { get; set; }
+} 
