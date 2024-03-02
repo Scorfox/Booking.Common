97a828047451acfb32ff82af1811f3c374c68531
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-03-02 10:37:34 +0500

    feat: Добавила контракты по аутентификации, авторизации

---
+++ Booking.Contracts/Auth/Requests/Authenticate.cs
@@ -0,0 +1,8 @@
+﻿namespace Otus.Booking.Common.Booking.Contracts.Auth.Requests;
+
+// Запрос на аутентификакцию, авторизацию и получения JWT-токена
+public record Authenticate
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ Booking.Contracts/Auth/Responses/AuthenticateResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Otus.Booking.Common.Booking.Contracts.Auth.Responses;
+
+// Ответ на запрос аутентификации, авторизации
+public record AuthenticateResult
+{
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+}
