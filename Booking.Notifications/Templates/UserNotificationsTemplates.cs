b600fd40296b146735d5a0bf98b19bad4beed972
fangarh <dvornikov.d@gmail.com>
2024-03-18 09:39:07 +0300

    Update UserNotificationsTemplates.cs

--- Booking.Notifications/Templates/UserNotificationsTemplates.cs
+++ Booking.Notifications/Templates/UserNotificationsTemplates.cs
@@ -2,6 +2,16 @@
 {
     public record UserNotificationsTemplates
     {
-        public static string UserCreatedTemplate => "";
+        public static string UserCreatedTemplate => @"
+            Добрый день @Model.Name @Model.LastName!
+
+            Вы успешно зарегистрировались в системе Booking Entertaimont.
+@if (!String.IsNullOrEmpty(Model.Company))
+    {
+<p>Вы зарегистрированы как сотрудник @Model.Company</p>
+
+    }
+            Ваш логином: @Model.Login.
+        ";
     }
 }
