f41a7e6bdcb1f965954cd9696e3ea5a94fe09215
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-03-18 13:03:47 +0500

    feat: Убрала удаление бронирования, добавила ForbiddenException

--- Booking.Contracts/Reservation/Requests/DeleteReservation.cs
+++
@@ -1,8 +0,0 @@
-﻿using Otus.Booking.Common.Booking.Contracts.Common;
-
-namespace Otus.Booking.Common.Booking.Contracts.Reservation.Requests;
-
-/// <summary>
-/// Запрос на удаление бронирования
-/// </summary>
-public record DeleteReservation : RequestById;
--- Booking.Contracts/Reservation/Responses/DeleteReservationResult.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Otus.Booking.Common.Booking.Contracts.Reservation.Responses;
-
-/// <summary>
-/// Ответ на успешное удаление брони
-/// </summary>
-public record DeleteReservationResult;
